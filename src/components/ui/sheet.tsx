b9745292214ab6ec70ab7acad895cb4bafd07e49
stude-md <studymaewq@gmail.com>
2025-02-05 18:56:26 +0530

    Fix ESLint errors for Vercel deployment

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -33,9 +33,9 @@ const SheetContent = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Content> & {
     side?: "top" | "right" | "bottom" | "left"
   }
->(({ side = "right", className, ...props }, ref) => (
-  <SheetPortal>
-    <SheetOverlay />
+>(({ side = "right", className, ...props }, ref) => {
+  const { children, ...restProps } = props
+  const content = (
     <SheetPrimitive.Content
       ref={ref}
       className={cn(
@@ -52,15 +52,23 @@ const SheetContent = React.forwardRef<
         },
         className
       )}
-      {...props}
+      {...restProps}
     >
+      {children}
       <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
         <LuX className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
     </SheetPrimitive.Content>
-  </SheetPortal>
-))
+  )
+
+  return (
+    <SheetPortal>
+      <SheetOverlay />
+      {content}
+    </SheetPortal>
+  )
+})
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
 const SheetHeader = ({
