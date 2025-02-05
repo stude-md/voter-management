d774f8d7d24ffd23a24bf10c6e43abec1c8e84c7
stude-md <studymaewq@gmail.com>
2025-02-05 19:31:38 +0530

    Fix remaining ESLint errors

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -33,7 +33,7 @@ type SheetContentProps = React.ComponentPropsWithoutRef<typeof SheetPrimitive.Co
 }
 
 const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Content>, SheetContentProps>(
-  ({ side = "right", className, children, ...props }, ref) => (
+  ({ side = "right", className, ...props }, ref) => (
     <SheetPortal>
       <SheetOverlay />
       <SheetPrimitive.Content
@@ -54,7 +54,6 @@ const SheetContent = React.forwardRef<React.ElementRef<typeof SheetPrimitive.Con
         )}
         {...props}
       >
-        <div>{children}</div>
         <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
           <LuX className="h-4 w-4" />
           <span className="sr-only">Close</span>
