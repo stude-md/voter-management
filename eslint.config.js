4d94be92ee739cb2a10e10970abeee5c296c822c
stude-md <studymaewq@gmail.com>
2025-02-28 17:58:17 +0530

    Remove unused variable in sheet.tsx

---
+++ eslint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // ...existing configuration...
+  ignores: [
+    'node_modules/',
+    'build/',
+    // Add other paths you want to ignore
+  ],
+  // ...existing configuration...
+};
