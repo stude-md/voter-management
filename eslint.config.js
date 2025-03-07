5dcd6e3b298f2a5544710cf664c8e08a1fd396ed
stude-md <studymaewq@gmail.com>
2025-03-07 13:43:27 +0530

    Fix ESLint config and toast utility

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,11 @@
 // eslint.config.js
-import { defineConfig } from 'eslint-define-config';
-
-export default defineConfig({
-  // Add ignores property
+module.exports = {
+  extends: ["next/core-web-vitals"],
   ignores: [
     'node_modules',
     '.next',
     'public',
     'dist',
     '.git'
-  ],
-  rules: {
-    // Your existing rules
-  },
-  extends: [
-    "next/core-web-vitals"
   ]
-});
+};
